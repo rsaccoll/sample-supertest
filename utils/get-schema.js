84fad8b764cc3e2375a99d8026eb017df7967502
Ramses Almeida <rsaccoll@gmail.com>
2019-09-18 21:24:15 -0300

    adjust schema

--- utils/get-schema.js
+++ utils/get-schema.js
@@ -1,18 +1,24 @@
 const Joi = require('@hapi/joi')
 const validation = require('./validations/rest_validations');
 
+const acceptEnconding = 'Accept-Encoding'
+const acceptLanguage = 'Accept-Language'
+const secFetchMode = 'Sec-Fetch-Mode'
+const secFetchSite = 'Sec-Fetch-Site'
+const userAgent = 'User-Agent'
+
 const get_schema = Joi.object().keys({
-    'args': validation.STRING_NOT_NULL.required(),
-    'headers': Joi.object().keys({
-        'Accept': validation.STRING_NOT_NULL.required(),
-        'Accept-Encoding': validation.STRING_NOT_NULL.required(),
-        'Accept-Language': validation.STRING_NOT_NULL.required(),
-        'Host': validation.STRING_NOT_NULL.required(),
-        'Referer': validation.STRING_NOT_NULL.required(),
-        'Sec-Fetch-Mode': validation.STRING_NOT_NULL.required(),
-        'Sec-Fetch-Site': validation.STRING_NOT_NULL.required(),
-        'User-Agent': validation.STRING_NOT_NULL.required()
+    args: '',
+    headers: Joi.object().keys({
+        Accept: validation.STRING_NOT_NULL.required(),
+        acceptEnconding: validation.STRING_NOT_NULL.required(),
+        acceptLanguage: validation.STRING_NOT_NULL.required(),
+        Host: validation.STRING_NOT_NULL.required(),
+        Referer: validation.STRING_NOT_NULL.required(),
+        secFetchMode: validation.STRING_NOT_NULL.required(),
+        secFetchSite: validation.STRING_NOT_NULL.required(),
+        userAgent: validation.STRING_NOT_NULL.required()
     }),
-    'origin': validation.IP.required(),
-    'url': validation.STRING_NOT_NULL.required()
+    origin: validation.IP.required(),
+    url: validation.STRING_NOT_NULL.required()
 })
