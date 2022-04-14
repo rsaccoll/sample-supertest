1a36c431c0af2ff9083e7fe641fe5be022971160
Ramses Almeida <rsaccoll@gmail.com>
2022-04-14 14:04:12 -0300

    Update get-schema.js

--- utils/get-schema.js
+++ utils/get-schema.js
@@ -3,23 +3,25 @@ const validation = require('./validations/rest_validations');
 
 const acceptEnconding = 'Accept-Encoding'
 const acceptLanguage = 'Accept-Language'
+const SecFetchDest = 'Sec-Fetch-Dest'
 const secFetchMode = 'Sec-Fetch-Mode'
 const secFetchSite = 'Sec-Fetch-Site'
+const SecChUa = 'Sec-Ch-Ua'
+const SecChUaPlatform = 'Sec-Ch-Ua-Platform'
 const userAgent = 'User-Agent'
+const XAmznTraceId = 'X-Amzn-Trace-Id'
+const ContentType = 'Content-Type'
 
 const get_schema = Joi.object().keys({
     args: '',
     headers: Joi.object().keys({
-        Accept: validation.STRING_NOT_NULL.required(),
         acceptEnconding: validation.STRING_NOT_NULL.required(),
-        acceptLanguage: validation.STRING_NOT_NULL.required(),
+        ContentType: validation.STRING_NOT_NULL.required(),
         Host: validation.STRING_NOT_NULL.required(),
-        Referer: validation.STRING_NOT_NULL.required(),
-        secFetchMode: validation.STRING_NOT_NULL.required(),
-        secFetchSite: validation.STRING_NOT_NULL.required(),
-        userAgent: validation.STRING_NOT_NULL.required()
+        userAgent: validation.STRING_NOT_NULL.required(),
+        XAmznTraceId: validation.STRING_NOT_NULL.required()
     }),
-    origin: validation.IP.required(),
+    origin: validation.STRING_NOT_NULL.required(),
     url: validation.STRING_NOT_NULL.required()
 })
 
